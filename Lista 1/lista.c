6a90c25965a5432e33c6c42bd763dc426652b1b9
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2021-06-14 16:29:58 -0300

    .

--- Lista 1/lista.c
+++ Lista 1/lista.c
@@ -1,15 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
+typedef struct Lista
+{
+    int elem;
+    struct Lista *ptr;
+} Lista;
+
+int fibonacci(int n);//ex1
+
+
+Lista *inicializa(void);//ex 5
+void imprime(Lista *l); //ex 5
+//função 1
+Lista *insere_inicio(Lista *lista, int elemento);//ex 5
+//função 2
+Lista *insere_final(Lista *lista, int elemento);//ex 5
+
+int main()
+{
+    //-----------------------Exercício 5--------------------------------
+    Lista *lista = inicializa(); //inicializa lista
+    lista = insere_inicio(lista, 4);
+    lista = insere_inicio(lista, 3);
+    lista = insere_inicio(lista, 2);
+    lista = insere_inicio(lista, 1);
+    lista = insere_final(lista, 5);
+    imprime(lista);
+}
 
 //Exercício 1
-int fibonacci(int n);
 
-int fibonacci(int n){
+int fibonacci(int n)
+{
     int a = 0;
     int b = 1;
-    int aux =0;
+    int aux = 0;
 
-    for(int i=2; i<n; i++){
+    for (int i = 2; i < n; i++)
+    {
         aux = b;
         b = a + b;
         a = aux;
@@ -17,21 +45,39 @@ int fibonacci(int n){
     return b;
 }
 
-//Exercício 5
+//----------------------Exercício 5-----------------------------------
 
-//função 1
-void insere_inicio (int elemento);
-
-//função 2
-void insere_final(int elemento);
-
-typedef struct Lista{
-    int elem;
-    struct Lista *ptr;
-}Lista;
-
-/*
-int main(){
-
+Lista *inicializa(void)
+{
+    return NULL;
 }
-*/
+
+Lista *insere_inicio(Lista *lista, int elemento)
+{
+    Lista *novo = (Lista *)malloc(sizeof(Lista));
+    novo->elem = elemento;
+    novo->ptr = lista;
+    return novo;
+}
+
+Lista *insere_final(Lista *lista, int elemento)
+{
+    Lista *novo = (Lista *)malloc(sizeof(Lista));
+    novo->elem = elemento;
+    novo->ptr = NULL;
+    Lista *aux = lista;
+
+    while (aux->ptr != NULL)
+    {
+        aux = aux->ptr;
+    }
+    aux->ptr = novo;
+    return lista;
+}
+
+void imprime(Lista *l)
+{
+    Lista *p; /* variável auxiliar para percorrer a lista */
+    for (p = l; p != NULL; p = p->ptr)
+        printf("info = %d\n", p->elem);
+}
